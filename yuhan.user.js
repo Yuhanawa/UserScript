0c64aeb59232d51fcbcb00c691a1ed47a0fcb523
Yuhanawa <3261817910@qq.com>
2023-02-03 00:42:43 +0800

    02-02 0.5.6.4 调整bing搜索推荐位置

--- yuhan.user.js
+++ yuhan.user.js
@@ -5,7 +5,7 @@
 // @name:en      Yuhan User Script
 // @name:en-US   Yuhan User Script
 // @namespace    http://github.com/yuhanawa/UserScript
-// @version      0.5.6.3
+// @version      0.5.6.4
 // @description  搜索引擎(百度 必应 谷歌 f搜)优化美化 搜索引擎快速切换 哔哩哔哩(bilibili B站)细节优化 视频快捷分享复制 移除评论区关键字搜索蓝字 CSDN极简化 CSDN沉浸式阅读 CSDN免登录复制 去除部分网站复制小尾巴 持续更新中
 // @description:zh  搜索引擎(百度 必应 谷歌 f搜)优化美化 搜索引擎快速切换 哔哩哔哩(bilibili B站)细节优化 视频快捷分享复制 移除评论区关键字搜索蓝字 CSDN极简化 CSDN沉浸式阅读 CSDN免登录复制 去除部分网站复制小尾巴 持续更新中
 // @description:en Search engine (Baidu Bing, Google f search) optimization and beautification of search engines, quick switching, Bilibili (bilibili B station), details, optimization, video, quick sharing, copying, removing comment area, keyword search, blue word CSDN, extremely simplified CSDN, immersive reading, CSDN free login Copy and remove some websites, copy the small tail, and continue to update
@@ -886,6 +886,15 @@
                 #b_header {
                     height: unset!important;
                 }
+                
+                #recentSearchLGContainer{
+                    top: 5px !important;
+                    right: 2px !important;
+                    left:unset !important;
+                }
+                #recentSearchLGContainer > h2::after{
+                    content:": 此处样式(配色,动画等)待适配:快懒死了,一点都不想干活"
+                }                         
                 `;
                 addClass(".item-awa", "#b_results > li")
 
