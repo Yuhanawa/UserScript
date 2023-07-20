878e7867e257f38fc0b605099fd1bf5343132a7a
Yuhanawa <3261817910@qq.com>
2023-07-20 18:56:42 +0800

    再修复

--- src/bilibili/src/widescreen.js
+++ src/bilibili/src/widescreen.js
@@ -3,35 +3,64 @@
         timeoutOnLoad(() => {
             const player = document.getElementById("bilibili-player");
             const playerWarp = document.getElementById("playerWrap");
-            const videoTitle = document.getElementsByClassName("video-title")[0];
+            const video = player.querySelector("video");
+            const updateText = document.getElementsByClassName("pubdate-text")[0];
+            let miniPlayerWarp = document.getElementsByClassName("bpx-player-mini-warp")[0];
 
-            let old_title = videoTitle.innerHTML
+            let old_size = {
+                height: playerWarp.getBoundingClientRect().height,
+                width: playerWarp.getBoundingClientRect().width
+            }
+
+            let old_text = updateText.innerHTML
             let old_position = "old";
+            let old_miniPlayerWarpDisplay = "none";
+            if (miniPlayerWarp != null) old_miniPlayerWarpDisplay = miniPlayerWarp.style.display
             delay(() => {
                 let position = window.getComputedStyle(player).position
-                if (old_position == position && old_title == videoTitle.innerHTML) return;
+                let miniPlayerWarpDisplay = "none";
+                miniPlayerWarp = document.getElementsByClassName("bpx-player-mini-warp")[0];
+                if (miniPlayerWarp != null) miniPlayerWarpDisplay = miniPlayerWarp.style.display
+                if (old_position == position && old_text == updateText.innerHTML && old_miniPlayerWarpDisplay == miniPlayerWarpDisplay) return;
 
-                old_position = position
-                old_title = videoTitle.innerHTML
-
-                if (position == "relative") {
-                    playerWarp.style.width = "120%"
-                    playerWarp.style.marginLeft = "7.2%"
+                if (miniPlayerWarpDisplay != 'none') {
+                    // 小窗
+                    playerWarp.style.height = old_size.height + "px";
+                    playerWarp.style.width = old_size.width + "px";
+                    video.style.maxHeight = '';
                 } else {
-                    playerWarp.style.width = "auto"
-                    playerWarp.style.marginLeft = "0"
-                }
-                // player.style.width = "auto"
-                player.style.height = "auto"
+                    playerWarp.style.height = 'auto';
+                    playerWarp.style.width = 'auto';
+                    if (player.classList.contains('mode-webscreen')) {
+                        // 网页全屏
+                        playerWarp.style.width = "auto"
+                        playerWarp.style.marginLeft = "0"
+                        player.style.width = "100%"
+                        video.style.maxHeight = '';
+                    } else if (position == "relative") {
+                        // 剧场模式
+                        playerWarp.style.width = "120%"
+                        playerWarp.style.marginLeft = "7.2%"
+                        player.style.width = "auto"
+                        video.style.maxHeight = '80vh';
+                    } else {
+                        // 默认
+                        playerWarp.style.width = "auto"
+                        playerWarp.style.marginLeft = "0"
+                        player.style.width = "100%"
+                        video.style.maxHeight = '80vh';
+                    }
 
-                setTimeout(() => {
-                    let width = Math.floor(player.getBoundingClientRect().width);
-                    if (width % 2 !== 0) width++;
-                    playerWarp.style.width = width + 'px';
-                    player.style.height = width/16*9 + 'px';
-                }, 200)
+                    old_position = position
+                    old_text = updateText.innerHTML
+                    old_miniPlayerWarpDisplay = miniPlayerWarpDisplay
+                    old_size = {
+                        height: playerWarp.getBoundingClientRect().height,
+                        width: playerWarp.getBoundingClientRect().width
+                    }
+                }
             }, 200, true)
-        }, 860)
+        }, 500)
         return $SASS(widescreen.sass)
     }, '已关闭$off': null,
 }
