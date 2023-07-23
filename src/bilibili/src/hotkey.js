c04b311858b0fcf2c7abb7d3468914574af76f94
Yuhanawa <3261817910@qq.com>
2023-07-23 23:10:30 +0800

    添加快捷键增强

---
+++ src/bilibili/src/hotkey.js
@@ -0,0 +1,16 @@
+快捷键增强, ["bilibili.com"], {
+    '已开启$on': () => {
+        if (location.href.match('www.bilibili.com/video')) {
+            intervalOnLoad(() => {
+                const img_view = document.querySelector('.reply-view-image')
+                if (img_view == undefined) return;
+
+                img_view.addEventListener('keydown', (e) => {
+                    if (e.key == 'Escape')  img_view.getElementsByClassName('close-container')[0].click();
+                    if (e.key == 'a' || e.key == 'ArrowLeft')  img_view.getElementsByClassName('last-image')[0].click();
+                    if (e.key == 'd' || e.key == 'ArrowRight')  img_view.getElementsByClassName('next-image')[0].click();
+                })
+            }, 400);
+        }
+    }, '已关闭$off': null,
+  }
