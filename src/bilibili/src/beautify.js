a53fefb9f33566eb2465b35cb8f821aeb9996181
Yuhanawa <3261817910@qq.com>
2023-07-25 22:33:24 +0800

    添加信息节食和首页关闭美化功能

--- src/bilibili/src/beautify.js
+++ src/bilibili/src/beautify.js
@@ -1,5 +1,7 @@
 样式美化 | 自定义背景 | 大幅度修改, ["www.bilibili.com"], {
   '已开启$on': () => {
+    if (location.href === 'https://www.bilibili.com/' && $get('bilibili_beautify_work_on_index', 'on') === 'off') return;
+
     if ($get('bilibili_eye_protection_cover', 'on') === 'on') {
       style('html,:root{--bodybackground: ' +
         (window.matchMedia("(prefers-color-scheme: dark)").matches
@@ -8,6 +10,10 @@
       )
     } else {
       style('html,:root{--bodybackground:transparent}')
+    }
+
+    if ($get('bilibili_information_diet', 'off') === 'on') {
+      style(`.container{max-height: ${$get('bilibili_information_diet_num', '8')}00vh;overflow: hidden;}`)
     }
 
     style(`html,:root{--background:${$get('bilibili_background_value', 'url(//s1.hdslb.com/bfs/static/stone-free/dyn-home/assets/bg.png)')}}`)
