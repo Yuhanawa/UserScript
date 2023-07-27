ce825a3ed0cc02b9f6395a1e1e8f869b89b27fab
Yuhanawa <3261817910@qq.com>
2023-07-28 00:05:16 +0800

    移除前缀 添加移除功能

--- src/csdn/src/toolbox.js
+++ src/csdn/src/toolbox.js
@@ -1,5 +1,10 @@
-CSDN底部不跟随, [/blog\.csdn\.net\/.*\/article\/details/], {
-  '已开启$on': () => {
+底部不跟随或移除, [/blog\.csdn\.net\/.*\/article\/details/], {
+  '移除$remove': () => {
+    return `.left-toolbox{
+          display: none!important;
+        }`
+  },
+  '不跟随$relative': () => {
     return `.left-toolbox{
           z-index: 996!important;
           left: 0px!important;
