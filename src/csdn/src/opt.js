cf4f3bec125dc1c2821d94de0bbb4d1b995850d5
Yuhanawa <3261817910@qq.com>
2023-07-28 17:44:57 +0800

    添加宽度调整

--- src/csdn/src/opt.js
+++ src/csdn/src/opt.js
@@ -1,4 +1,4 @@
-极简化, [/blog\.csdn\.net\/.*\/article\/details/],
+极简化, [/blog\.csdn\.net(\/.*)?\/article\/details./],
 {
     '已开启$on': () => {
         timeoutOnLoad(() => {
@@ -18,6 +18,12 @@
             // 删除不美观的冒号
             document.getElementsByClassName("blog-tags-box")[0].innerHTML = document.getElementsByClassName("blog-tags-box")[0].innerHTML.replaceAll("：", "");
         }, 100)
+
+        if ($get('csdn_width','on') === 'on') {
+            style(`#mainBox > main{ width: 100% !important; }`);
+            style(`body #mainBox{ width: ${$get('csdn_width_value','82')}% !important; }`);
+        }
+
         return $CSS(csdn.css)
     }, '已关闭$off': null
 }
