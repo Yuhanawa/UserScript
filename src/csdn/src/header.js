16237fa753d9fde73b7c1fdc7aa8320ce419829b
Yuhanawa <3261817910@qq.com>
2023-07-30 20:52:47 +0800

    合并顶部功能

--- src/csdn/src/header.js
+++ src/csdn/src/header.js
@@ -1,4 +1,6 @@
-顶部不跟随, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
-  '已开启$on': () => `#csdn-toolbar{position: static !important;}`,
+移除顶部或不跟随或淡化, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
+  '淡化不跟随$opacity': `#csdn-toolbar{position: static !important; opacity: 0.55;}`,
+  '不跟随$static': `#csdn-toolbar{position: static !important;}`,
+  '移除$remove': `#csdn-toolbar{ display: none!important; }`,
   '已关闭': null
 }
--- src/csdn/src/remove_header.js
+++
@@ -1,6 +0,0 @@
-移除顶部, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
-  已开启$on: () => {
-    return `#csdn-toolbar{ display: none!important; }`
-  },
-  已关闭$off: null,
-}
