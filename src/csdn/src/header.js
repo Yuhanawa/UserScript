6e9fec74086e04429f5c3a2ac8e69a91732cb162
Yuhanawa <3261817910@qq.com>
2023-07-30 20:54:28 +0800

    修改默认值

--- src/csdn/src/header.js
+++ src/csdn/src/header.js
@@ -1,6 +1,6 @@
 移除顶部或不跟随或淡化, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
+  '移除$remove': `#csdn-toolbar{ display: none!important; }`,
   '淡化不跟随$opacity': `#csdn-toolbar{position: static !important; opacity: 0.55;}`,
   '不跟随$static': `#csdn-toolbar{position: static !important;}`,
-  '移除$remove': `#csdn-toolbar{ display: none!important; }`,
   '已关闭': null
 }
