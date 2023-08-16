f5a4240ed00f37993c3504838d7acfbc3ec03022
Yuhanawa <3261817910@qq.com>
2023-08-16 23:47:59 +0800

    补充说明

--- dev.js
+++ dev.js
@@ -5,7 +5,7 @@
 // @author       Yuhanawa
 // @match        *://*/*
 // @require      file:///{PATH}/out/all.js
-// !!! 通过修改{PATH} 为当前目录, 将此脚本添加到脚本管理器(Tampermonkey)中, 并打开脚本管理器[允许访问文件网址]权限 !!!
+// !!! 通过修改{PATH} 为当前目录的绝对路径, 将此脚本添加到脚本管理器(Tampermonkey)中, 并打开脚本管理器[允许访问文件网址]权限 !!!
 // !例如@require  file:///E://UserScript/out/all.js
 // @run-at       document-start
 // @grant        GM_setValue
@@ -16,7 +16,7 @@
 // ==/UserScript==
 
 (function() {
-    // !!! 通过修改{PATH} 为当前目录, 将此脚本添加到脚本管理器(Tampermonkey)中, 并打开脚本管理器[允许访问文件网址]权限 !!!
-    // !!! 通过修改{PATH} 为当前目录, 将此脚本添加到脚本管理器(Tampermonkey)中, 并打开脚本管理器[允许访问文件网址]权限 !!!
-    // !!! 通过修改{PATH} 为当前目录, 将此脚本添加到脚本管理器(Tampermonkey)中, 并打开脚本管理器[允许访问文件网址]权限 !!!
+    // !!! 通过修改{PATH} 为当前目录的绝对路径, 将此脚本添加到脚本管理器(Tampermonkey)中, 并打开脚本管理器[允许访问文件网址]权限 !!!
+    // !!! 通过修改{PATH} 为当前目录的绝对路径, 将此脚本添加到脚本管理器(Tampermonkey)中, 并打开脚本管理器[允许访问文件网址]权限 !!!
+    // !!! 通过修改{PATH} 为当前目录的绝对路径, 将此脚本添加到脚本管理器(Tampermonkey)中, 并打开脚本管理器[允许访问文件网址]权限 !!!
 })();
