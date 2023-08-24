e5abb6675477ac846e15d23b531029da348a24f2
Yuhanawa <3261817910@qq.com>
2023-08-25 02:58:14 +0800

    添加更多grant

--- dev.js
+++ dev.js
@@ -1,5 +1,6 @@
 // ==UserScript==
 // @name         User Script 开发使用
+// @description  热更新
 // @namespace    http://github.com/yuhanawa/
 // @version      dev
 // @author       Yuhanawa
@@ -8,11 +9,27 @@
 // !!! 通过修改{PATH} 为当前目录的绝对路径, 将此脚本添加到脚本管理器(Tampermonkey)中, 并打开脚本管理器[允许访问文件网址]权限 !!!
 // !例如@require  file:///E://UserScript/out/all.js
 // @run-at       document-start
+// @grant        unsafeWindow
+// @grant        GM_addStyle
 // @grant        GM_setValue
 // @grant        GM_getValue
-// @grant        GM_addStyle
-// @grant        GM_setClipboard
+// @grant        GM_deleteValue
+// @grant        GM_listValues
+// @grant        GM_addValueChangeListener
+// @grant        GM_removeValueChangeListener
+// @grant        GM_getResourceText
+// @grant        GM_getResourceURL
 // @grant        GM_registerMenuCommand
+// @grant        GM_unregisterMenuCommand
+// @grant        GM_openInTab
+// @grant        GM_xmlhttpRequest
+// @grant        GM_download
+// @grant        GM_getTab
+// @grant        GM_saveTab
+// @grant        GM_getTabs
+// @grant        GM_notification
+// @grant        GM_setClipboard
+// @grant        GM_info
 // ==/UserScript==
 
 (function() {
