a23a79925ab4b5a957d4b98ce48395704ec6e287
Yuhanawa <3261817910@qq.com>
2023-08-25 17:21:41 +0800

    添加 @connect *

--- dev.js
+++ dev.js
@@ -9,6 +9,7 @@
 // !!! 通过修改{PATH} 为当前目录的绝对路径, 将此脚本添加到脚本管理器(Tampermonkey)中, 并打开脚本管理器[允许访问文件网址]权限 !!!
 // !例如@require  file:///E://UserScript/out/all.js
 // @run-at       document-start
+// @connect      *
 // @grant        unsafeWindow
 // @grant        GM_addStyle
 // @grant        GM_setValue
