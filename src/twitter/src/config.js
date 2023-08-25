4b843a0dcc12216bce407b2aebbf848b36f5dad7
Yuhanawa <3261817910@qq.com>
2023-08-25 21:12:31 +0800

    修复自动更新

--- src/twitter/src/config.js
+++ src/twitter/src/config.js
@@ -1,6 +1,7 @@
 $, ['yuhan-script-config.netlify.app', 'twitter.com', 'x.com'], () => {
     delay(async () => {
         let callback_num = 0;
+
         const getText = (url, callback) => {
             callback_num += 1
             GM_xmlhttpRequest({
@@ -15,6 +16,22 @@ $, ['yuhan-script-config.netlify.app', 'twitter.com', 'x.com'], () => {
                 }
             });
         }
+        const onupdate = (reload) => {
+            if (callback_num === 0) {
+                $set("twitter_feed_rule_cache_last_check", Date.now());
+                $set("twitter_feed_rule_cache", ruleObj);
+
+                if (reload) location.reload()
+
+                const btn = document.createElement('button')
+                btn.onclick = () => location.reload()
+                btn.className = "note-update"
+                btn.innerText = "屏蔽器规则更新完成|刷新即可生效|点击刷新"
+                document.body.insertAdjacentElement("beforeend", btn)
+            } else {
+                setTimeout(() => onupdate(reload), 1000);
+            }
+        }
 
         loadRule($get("twitter_user_rule"))
 
@@ -27,18 +44,25 @@ $, ['yuhan-script-config.netlify.app', 'twitter.com', 'x.com'], () => {
             $set("twitter_feed_rule_cache", {})
             ruleObj = {}
 
+            const btn = document.createElement('button')
+            btn.onclick = () => location.reload()
+            btn.className = "note-update"
+            btn.innerText = "屏蔽器规则已清空|正在重新获取规则"
+            btn.style.zIndex = '10086'
+            document.body.insertAdjacentElement("beforeend", btn)
+
 
             for (let url of feed.split('\n')) {
                 url = url.trim();
                 if (url.length === 0) continue
 
-                console.log(url);
                 getText(url, (str) => {
                     ruleObj[url] = str
                     $set("twitter_feed_rule_cache", ruleObj);
                 });
             }
 
+            setTimeout(() => onupdate(true), 1200);
             $set("twitter_feed_rule_cache_last_check", Date.now())
         }
         let lastCheckTime = $get("twitter_feed_rule_cache_last_check", 0)
@@ -46,31 +70,15 @@ $, ['yuhan-script-config.netlify.app', 'twitter.com', 'x.com'], () => {
         for (const key of Object.keys(ruleObj)) {
             loadRule(ruleObj[key]);
         }
-        for (const url of Object.keys(ruleObj)) {
-            // 一天检查一次
-            if (Date.now() - lastCheckTime > 24 * 60 * 60 * 1000) {
+        // 一天检查一次
+        if (Date.now() - lastCheckTime > 24 * 60 * 60 * 1000) {
+            for (const url of Object.keys(ruleObj)) {
                 getText(url, (str) => {
                     ruleObj[url] = str
                     $set("twitter_feed_rule_cache", ruleObj);
                 });
             }
+            setTimeout(() => onupdate(), 800);
         }
-        if (Date.now() - lastCheckTime > 24 * 60 * 60 * 1000) {
-            const save = () => {
-                if (callback_num === 0) {
-                    $set("twitter_feed_rule_cache_last_check", Date.now());
-                    $set("twitter_feed_rule_cache", ruleObj);
-
-                    const btn = document.createElement('button')
-                    btn.onclick = ()=>location.reload()
-                    btn.className = "note-update"
-                    btn.innerText = "屏蔽器规则更新完成|刷新即可生效|点击刷新"
-                    
-                    document.body.insertAdjacentElement("beforeend",btn)
-                } else {
-                    setTimeout(() => save(), 1000);
-                }
-            }
-        }
-    }, 200)
+    }, 250)
 }
