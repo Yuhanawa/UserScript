1cdec54d524379574f45ab9231ffd28ce1cda918
Yuhanawa <3261817910@qq.com>
2023-09-29 13:03:18 +0800

    移除测试脚本

--- src/.TestScript/main.js
+++
@@ -1,3 +0,0 @@
-// 这个文件不会被格式化
-
-console.log("Test main.js");
--- src/.TestScript/src/style_test.js
+++
@@ -1,8 +0,0 @@
-样式测试, [/./],
-{
-    已关闭$off: null,
-    已开启$on: () => {
-        console.log("!!!!!!!! TEST !!!!!!!!!");
-        return $SASS(test) + $CSS(test)
-    }
-}
