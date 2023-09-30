d2dfa6d033c3b2452491bdf3b9813ae71dd9cdcc
Yuhanawa <3261817910@qq.com>
2023-09-30 12:26:05 +0800

    添加表单联动及图片和颜色选择器

--- src/csdn/src/background.js
+++ src/csdn/src/background.js
@@ -1,7 +1,7 @@
 自定义背景, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
     '已关闭$off': null,
     '已开启$on': () => { 
-        style(`body{--background:${$get('csdn-background-value','url("https://csdnimg.cn/release/blogv2/dist/pc/themesSkin/skin-code/images/bg.png?v20200831")')}}`)
+        style(`body{--background:url("${$get('csdn-background-value')}")}`)
         style(`body{--blog-content-box-background:${$get('csdn-blog-content-box-background-value','rgb(245 246 247 / 90%)')}}`)
         style(`body{--blog-content-box-opacity:${$get('csdn-blog-content-box-opacity-value','0.98')}}`)
         style(`body{--blog-header-box-background:${$get('csdn-blog-header-box-background-value','transparent')}}`)
