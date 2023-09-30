97ced5df59f367dcfc9c10ffb6ef8e412fe17917
Yuhanawa <3261817910@qq.com>
2023-10-01 01:35:45 +0800

    从配置文件获取默认值

--- src/csdn/src/background.js
+++ src/csdn/src/background.js
@@ -1,10 +1,12 @@
 自定义背景, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
     '已关闭$off': null,
-    '已开启$on': () => { 
-        style(`body{--background:url("${$get('csdn-background-value')}")}`)
-        style(`body{--blog-content-box-background:${$get('csdn-blog-content-box-background-value','rgb(245 246 247 / 90%)')}}`)
-        style(`body{--blog-content-box-opacity:${$get('csdn-blog-content-box-opacity-value','0.98')}}`)
-        style(`body{--blog-header-box-background:${$get('csdn-blog-header-box-background-value','transparent')}}`)
-        return $SASS(background) 
+    '已开启$on': () => {
+        if ($get('csdn_background-value')) {
+            style(`body{background:url("${$get('csdn_background-value')}")}`)
+        }
+        style(`body{--blog-content-box-background:${$get('csdn_blog-content-box-background-value')}}`)
+        style(`body{--blog-content-box-opacity:${$get('csdn_blog-content-box-opacity-value', '0.98')}}`)
+        style(`body{--blog-header-box-background:${$get('csdn_blog-header-box-background-value')}}`)
+        return $SASS(background)
     },
 }
