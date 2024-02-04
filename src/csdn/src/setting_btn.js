a14a74c293acbbba7bf66acd38bc7b1c70f92014
Yuhanawa <3261817910@qq.com>
2024-02-04 23:42:26 +0800

    修改设置页地址

--- src/csdn/src/setting_btn.js
+++ src/csdn/src/setting_btn.js
@@ -4,7 +4,7 @@
             const articleTitleBox = document.getElementsByClassName('article-title-box')[0];
             const settingButton = document.createElement('a');
             settingButton.innerText = "脚本设置"
-            settingButton.href = 'https://yuhan-script-config.netlify.app';
+            settingButton.href = 'https://yuhanawa.github.io/tools/userscriptconfig/';
             settingButton.target = '_blank';
             settingButton.style = `float: right;margin: 12px;font-size: 20px;text-decoration: underline !important;color: #4ea1db;`
             articleTitleBox.insertAdjacentElement("afterbegin", settingButton);
