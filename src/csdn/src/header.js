2af5498bc604f5a2c87e0ad94f159516286e5438
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-02-08 21:26:53 +0800

    添加半透明选项

--- src/csdn/src/header.js
+++ src/csdn/src/header.js
@@ -1,6 +1,7 @@
-移除顶部或不跟随或淡化, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
+顶部菜单, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
   '移除$remove': `#csdn-toolbar{ display: none!important; }`,
-  '淡化不跟随$opacity': `#csdn-toolbar{position: static !important; opacity: 0.55;}`,
+  '半透明$opacity': `#csdn-toolbar{opacity: 0.55;}`,
+  '淡化不跟随$opacity_static': `#csdn-toolbar{position: static !important; opacity: 0.55;}`,
   '不跟随$static': `#csdn-toolbar{position: static !important;}`,
-  '已关闭$off': null
+  '显示$off': null
 }
--- src/csdn/src/toolbox.js
+++ src/csdn/src/toolbox.js
@@ -1,4 +1,4 @@
-底部不跟随或移除, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
+低部菜单工具栏, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
   '移除$remove': () => {
     return `.left-toolbox{
           display: none!important;
@@ -13,5 +13,20 @@
           position: relative!important;
         }`
   },
-  '已关闭$off': null
+  '半透明$opacity': () => {
+    return `.left-toolbox{
+          opacity: 0.55!important;
+        }`
+  },
+  '淡化不跟随$opacity_relative': () => {
+    return `.left-toolbox{
+          z-index: 996!important;
+          left: 0px!important;
+          bottom: 0px!important;
+          width: 900px!important;
+          position: relative!important;
+          opacity: 0.55!important;
+        }`
+  },
+  '显示$off': null
 }
