1e87627975bf7cb9a42e6e3957fb6b58dc7bce57
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-02-08 21:29:20 +0800

    添加子菜单支持

--- config-component/src/components/Line.jsx
+++ config-component/src/components/Line.jsx
@@ -1,5 +1,10 @@
 const Line = (props) => {
-    return (<hr id={props.id} className="line" />);
+    return (
+        <hr
+            id={props.id.substring(props.id.startsWith('#'))}
+            className="line"
+        />
+    );
 };
 
 export default Line;
