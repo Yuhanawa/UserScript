64d6f043b532c0a486009edeea4631098b7222a9
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-02-08 21:29:35 +0800

    添加表单排版选项

--- config-component/src/components/FromMain.jsx
+++ config-component/src/components/FromMain.jsx
@@ -5,14 +5,32 @@ import { useState } from 'react';
 import Line from './Line.jsx';
 import Twitter_user_rule_editor from './Twitter_user_rule_editor.jsx';
 import CSDN_UI_editor from './CSDN_UI_editor.jsx';
-import { Button } from 'antd';
-
+import { Button, Anchor, Form, Radio } from 'antd';
 
 
 const FromMain = ({ menuKey }) => {
     const form = useForm();
     let winProps = {};
     const [schema, setSchema] = useState({});
+    const [anchors, setAnchors] = useState([]);
+
+    const [fromColumn, setFromColumn] = useState(2);
+    const [fromDisplayType, setFromDisplayType] = useState('column');
+
+    const handFromColumnChange = (ev) => {
+        const value = ev.target.value;
+        schema.column = value;
+
+        form.setSchema(schema, true);
+        setFromColumn(value);
+    };
+    const handFromDisplayType = (ev) => {
+        const value = ev.target.value;
+        schema.displayType = value;
+        form.setSchema(schema, true);
+        setFromDisplayType(value);
+    };
+    
 
     if (window.userscript?.[menuKey])
         winProps = window.userscript[menuKey].props;
@@ -48,6 +66,15 @@ const FromMain = ({ menuKey }) => {
 
         console.log(newSchema);
         setSchema(newSchema)
+
+        setAnchors([
+            {
+                key: 'top',
+                href: `#top`,
+                title: 'Top',
+            }
+        ].concat(window.userscript?.[menuKey]?.anchors || []));
+
         // eslint-disable-next-line react-hooks/exhaustive-deps
     }, [menuKey])
 
@@ -55,7 +82,7 @@ const FromMain = ({ menuKey }) => {
         console.log('formData:', data);
 
         for (const key of Object.keys(data)) {
-            if (key.startsWith('.')) continue;
+            if (key.startsWith('.')||key.startsWith('#')) continue;
 
             const value = data[key]
             // biome-ignore lint/suspicious/noDoubleEquals: <explanation>
@@ -68,15 +95,39 @@ const FromMain = ({ menuKey }) => {
 
     };
 
-    const onSaveBtnClick = () => {
-        form.submit();
-    };
-    const onCancelBtnClick = () => {
-        location.reload();
-    };
+    const onSaveBtnClick = () => form.submit();
+    const onCancelBtnClick = () => location.reload();
+  
 
     return (
         <>
+            <div id='top' style={{ padding: "0 0 18px 0", margin: "-2px" }}>
+                <div style={{ backgroundColor: "#FCFCFC", padding: "8px", borderRadius: "8px" }}>
+                    <Anchor
+                        direction="horizontal"
+                        items={anchors}
+                        affix={true} /* don't work */
+                        offsetTop={"16px"}
+                    />
+                </div>
+            </div>
+
+            <div>
+                <Form.Item label='表单排版' style={{ margin: '0px 1px 24px' }}>
+                    <Radio.Group value={fromDisplayType} onChange={handFromDisplayType} style={{ margin: "0px 6px" }}>
+                        <Radio.Button value='row'>row</Radio.Button>
+                        <Radio.Button value='column'>column</Radio.Button>
+                        <Radio.Button value='inline'>Inline</Radio.Button>
+                    </Radio.Group>
+                    <Radio.Group value={fromColumn} onChange={handFromColumnChange} style={{ margin: "0px 6px" }}>
+                        <Radio.Button value={1}>一列</Radio.Button>
+                        <Radio.Button value={2}>两列</Radio.Button>
+                        <Radio.Button value={3}>三列</Radio.Button>
+                        <Radio.Button value={4}>四列</Radio.Button>
+                    </Radio.Group>
+                </Form.Item>
+            </div>
+
             <FormRender
                 form={form}
                 schema={schema}
