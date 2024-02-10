5b092a8fb0e5fb95da45a0217f3de1caa7223da5
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-02-10 19:07:35 +0800

    添加动画

--- src/csdn/src/toolbox.js
+++ src/csdn/src/toolbox.js
@@ -1,32 +1,36 @@
 低部菜单工具栏, [/blog\.csdn\.net(\/.*)?\/article\/details./], {
-  '移除$remove': () => {
-    return `.left-toolbox{
+  '移除$remove':
+    `.left-toolbox{
           display: none!important;
-        }`
-  },
-  '不跟随$relative': () => {
-    return `.left-toolbox{
+        }`,
+  '不跟随$relative':
+    `.left-toolbox{
           z-index: 996!important;
           left: 0px!important;
           bottom: 0px!important;
           width: 900px!important;
           position: relative!important;
-        }`
-  },
-  '半透明$opacity': () => {
-    return `.left-toolbox{
+        }`,
+  '半透明$opacity':
+    `.left-toolbox{
           opacity: 0.55!important;
-        }`
-  },
-  '淡化不跟随$opacity_relative': () => {
-    return `.left-toolbox{
+          transition: opacity 0.5s!important;
+        }
+        .left-toolbox:hover{
+          opacity: 1!important;
+        }`,
+  '淡化不跟随$opacity_relative':
+    `.left-toolbox{
           z-index: 996!important;
           left: 0px!important;
           bottom: 0px!important;
           width: 900px!important;
           position: relative!important;
           opacity: 0.55!important;
-        }`
-  },
+          transition: opacity 1.5s!important;
+        }
+        .left-toolbox:hover{
+          opacity: 1!important;
+        }`,
   '显示$off': null
 }
