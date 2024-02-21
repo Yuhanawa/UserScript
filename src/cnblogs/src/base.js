f510791dd4d5c7be2dcc07af681c918899e8ad32
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-02-22 02:00:39 +0800

    修改match为正则匹配

--- src/cnblogs/src/base.js
+++ src/cnblogs/src/base.js
@@ -1,6 +1,7 @@
 name: 美化
 match: 
-    - 'www.cnblogs.com'
+    - /www.cnblogs.com\/[^\/]*$/
+    - /www.cnblogs.com\/(pick|candidate|subscription|following|aggsite|cate|comment)\//
 directlyRun: true
 ,
 $SASS(base)
