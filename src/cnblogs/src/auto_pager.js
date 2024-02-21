f36e2841b3a8d92d3a2bd7297afb7623f43fc2c8
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-02-22 02:00:18 +0800

    提取nextpage函数

--- src/cnblogs/src/auto_pager.js
+++ src/cnblogs/src/auto_pager.js
@@ -5,7 +5,7 @@ directlyRun: true
     ,
     () => {
         timeoutOnLoad(() => {
-            if (!document.querySelector(".pager")||document.querySelector("#Autopage_number")) return
+            if (!document.querySelector(".pager") || document.querySelector("#Autopage_number")) return
 
             var timeout = 0
             setInterval(() => { if (timeout > 0) timeout-- }, 1000);
@@ -18,21 +18,11 @@ directlyRun: true
             }, 2000);
 
 
-            function nextPage () {
+            function nextPage() {
                 if (timeout > 0) return
                 timeout = 3;
 
-                GM_xmlhttpRequest({
-                    url: document.querySelector(".pager > a:nth-last-child(1)").href,
-                    method: 'GET',
-                    overrideMimeType: `text/html; charset=${document.characterSet || document.charset || document.inputEncoding}`,
-                    headers: {
-                        'x-requested-with': 'XMLHttpRequest',
-                        'Referer': location.href,
-                        'User-Agent': navigator.userAgent,
-                        'Accept': 'text/html,application/xhtml+xml,application/xml'
-                    },
-                    timeout: 10000,
+                getPage(document.querySelector(".pager > a:nth-last-child(1)").href, {
                     onload: (response) => {
                         try {
                             const doc = (new DOMParser()).parseFromString(response.responseText, "text/html");//"text/html"
@@ -45,14 +35,40 @@ directlyRun: true
                         } catch (e) {
                             console.error('ERR', e, response.responseText);
                         }
-                    },
-                    onerror: (response) => {
-                        console.error(`ERR: URL:${url}`, response);
-                    },
-                    ontimeout: (response) => {
-                        console.warn(`TIMEOUT: URL:${url}`, response);
                     }
-                });
+                })
+
+                // GM_xmlhttpRequest({
+                //     url: document.querySelector(".pager > a:nth-last-child(1)").href,
+                //     method: 'GET',
+                //     overrideMimeType: `text/html; charset=${document.characterSet || document.charset || document.inputEncoding}`,
+                //     headers: {
+                //         'x-requested-with': 'XMLHttpRequest',
+                //         'Referer': location.href,
+                //         'User-Agent': navigator.userAgent,
+                //         'Accept': 'text/html,application/xhtml+xml,application/xml'
+                //     },
+                //     timeout: 10000,
+                //     onload: (response) => {
+                //         try {
+                //             const doc = (new DOMParser()).parseFromString(response.responseText, "text/html");//"text/html"
+                //             const articles = doc.querySelectorAll('#post_list>article')
+                //             for (const article of articles) {
+                //                 document.querySelector('#post_list').insertAdjacentElement('beforeend', article)
+                //             }
+                //             document.querySelector('.pager').parentNode.replaceChild(doc.querySelector('.pager'), document.querySelector('.pager'))
+
+                //         } catch (e) {
+                //             console.error('ERR', e, response.responseText);
+                //         }
+                //     },
+                //     onerror: (response) => {
+                //         console.error(`ERR: URL:${url}`, response);
+                //     },
+                //     ontimeout: (response) => {
+                //         console.warn(`TIMEOUT: URL:${url}`, response);
+                //     }
+                // });
             }
         }, 400)
     }
