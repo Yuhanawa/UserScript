33628bac7f9120ccdf75efd716cc8b047ec0484d
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-04-03 22:55:24 +0800

    修改样式 监听元素以实时修改高度

--- src/cnblogs/src/ing_in_iframe.js
+++ src/cnblogs/src/ing_in_iframe.js
@@ -1,20 +1,46 @@
-name: sidenav
+name: ing_in_iframe
 match: ['/ing.cnblogs.com/']
 directlyRun: true
     ,
     () => {
-        if (top === self) return;
-
-        var style = $SASS(ing_in_iframe)
+        var style = $SASS(ing)
 
 
-        timeoutOnLoad(() => {
+        if (top === self) return style;
+
+        var style = style + $SASS(ing_in_iframe)
+        var refreshHeight = () => {
             console.log(document.documentElement);
             unsafeWindow.parent.postMessage({
                 type: "resizeIframe",
-                height: document.body.clientHeight ?? document.body.scrollHeight
+                height: document.body.scrollHeight ?? document.body.clientHeight + 220
             }, "*")
-        }, 250);
+        }
+
+        const observer = new MutationObserver(function (mutations) {
+            mutations.forEach(function (mutation) {
+                console.log(mutation);
+                // 遍历所有变化
+                if (mutation.type === 'childList') {
+                    refreshHeight()
+                } else if (mutation.type === 'attributes') {
+                    // 属性变化
+                }
+            });
+        });
+        onload(() => {
+            refreshHeight()
+
+            // 观察 #main 元素
+            observer.observe(document.getElementById('main'), {
+                childList: true,
+                attributes: false,
+                subtree: true,
+            });
+        })
+
+
+
 
         return style
     }
