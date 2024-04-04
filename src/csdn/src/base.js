a0b3162e24322b855c8709b80abaaade9b67ac82
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-04-05 02:41:05 +0800

    移除可能存在问题的代码

--- src/csdn/src/base.js
+++ src/csdn/src/base.js
@@ -1,22 +1,9 @@
 $, ['csdn.net'], () => {
-    // 可能存在问题，临时删除
+
     if ($get('csdn_beautify', true)) {
         if (false) {
-        timeoutOnLoad(() => {
-            // 移动文字标签位置
-            const taghtml = document.getElementsByClassName("blog-tags-box")[0].outerHTML + "";
-            document.getElementsByClassName("blog-tags-box")[0].remove();
-            document.getElementsByClassName("article-bar-top")[0].innerHTML = document.getElementsByClassName("article-bar-top")[0].innerHTML + taghtml;
-            // 简介时间格式
-            document.getElementsByClassName("time")[0].innerHTML = document.getElementsByClassName("time")[0].innerHTML
-                .replace("于&nbsp;", "").replace("&nbsp;发布", "");
-            // 刷新底部工具栏位置 使其居中
-            document.getElementsByClassName("left-toolbox")[0].style.left = "auto";
-            // 删除不美观的冒号
-            document.getElementsByClassName("blog-tags-box")[0].innerHTML = document.getElementsByClassName("blog-tags-box")[0].innerHTML.replaceAll("：", "");
-        }, 100)            
+            // 可能存在问题，临时删除
         }
-
     }
 
     fn = () => {
