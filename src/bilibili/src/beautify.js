8ad0fd765e79cd1b5f5f6fc11134ee9aa49beaa6
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-04-05 13:17:34 +0800

    抽离视频圆角功能

--- src/bilibili/src/beautify.js
+++ src/bilibili/src/beautify.js
@@ -1,19 +1,24 @@
-样式美化 | 自定义背景 | 大幅度修改, ["www.bilibili.com"], {
+(美化总开关)样式美化 & 自定义背景等, ["www.bilibili.com"], {
   '已开启$on': () => {
     if (location.href === 'https://www.bilibili.com/' && $get('bilibili_beautify_work_on_index', 'on') === 'off') return;
 
     if ($get('bilibili_eye_protection_cover', 'on') === 'on') {
       style('html,:root{--bodybackground: ' +
         (window.matchMedia("(prefers-color-scheme: dark)").matches
-        ? $get('bilibili_eye_protection_cover_dark', `rgb(0 0 0 / 23%)`)
-        : $get('bilibili_eye_protection_cover_light', `rgb(102 204 255 / 23%)`)) + '}'
+          ? $get('bilibili_eye_protection_cover_dark', `rgb(0 0 0 / 23%)`)
+          : $get('bilibili_eye_protection_cover_light', `rgb(102 204 255 / 23%)`)) + '}'
       )
     } else {
       style('html,:root{--bodybackground:transparent}')
     }
 
-    style(`html,:root{--background:url(${$get('bilibili_background_value')})}`)
-
+    if ($get('bilibili_video_radius', 'on') === 'on') {
+      style($SASS('beautify.video.sass'))
+    }
+    if ($get('bilibili_background', 'on') === 'on') {
+      style($SASS('beautify.background.sass'))
+      style(`html,:root{--background:url(${$get('bilibili_background_value')})}`)
+    }
     return $SASS('beautify.sass')
   }, '已关闭$off': null,
 }
