ad2d0073717d2f90c8fd44b5f382622ac0f70eeb
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-05-02 00:23:20 +0800

    优化代码

--- src/bilibili/src/video_cover_download.js
+++ src/bilibili/src/video_cover_download.js
@@ -1,29 +1,15 @@
-视频封面下载, ["www.bilibili.com/video", "www.bilibili.com/read"], {
+视频封面获取按钮, ["www.bilibili.com/video"], {
     '已开启$on': () => {
         intervalAfterLoad(() => {
             const toolbar = document.querySelector('#arc_toolbar_report .video-toolbar-right:not([video_cover])');
-            if (!toolbar) {
-                return;
-            }
+            if (!toolbar) return;
 
             toolbar.setAttribute('video_cover', 'true');
-
-            // const btn = document.createElement('div');
-            // btn.className = 'video-toolbar-right-item';
-            // btn.innerHTML = '<span class="video-toolbar-item-text">下载封面</span>';
-            // toolbar.insertBefore(btn, toolbar.firstChild);
-
-            // btn.addEventListener('click', () => {
-            //     // GM_download(unsafeWindow.__INITIAL_STATE__.videoData.pic);
-            // });
-
             const btn = document.createElement('div');
             btn.className = 'video-toolbar-right-item';
-            btn.innerHTML = `<a class="video-toolbar-item-text" target="_blank" href="${unsafeWindow.__INITIAL_STATE__.videoData.pic}">下载封面</a>`;
+            btn.innerHTML = `<a class="video-toolbar-item-text" target="_blank" href="${unsafeWindow.__INITIAL_STATE__.videoData.pic}">获取封面</a>`;
             toolbar.insertBefore(btn, toolbar.firstChild);
-
-
-        }, 8000);
+        }, 8000, true);
     },
     '已关闭$off': null,
 }
