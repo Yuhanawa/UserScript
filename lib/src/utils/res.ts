ddb348146600ea5c8c9ad22068a9c561f0fb509d
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-06-09 01:57:36 +0800

    style: format

--- lib/src/utils/res.ts
+++ lib/src/utils/res.ts
@@ -1,8 +1,11 @@
 import fs from "fs-extra";
-import path from "path";
+import path from "node:path";
 
-function get_core_utils() : string {    
-    return fs.readFileSync(path.resolve(__dirname,"..","..","..","res/core_utils.js"), "utf8");
+function get_core_utils(): string {
+	return fs.readFileSync(
+		path.resolve(__dirname, "..", "..", "..", "res/core_utils.js"),
+		"utf8",
+	);
 }
 
-export default { get_core_utils }
+export default { get_core_utils };
