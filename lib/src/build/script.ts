33883eaac802e6099960583aa2f95dbe349cc1ae
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-06-08 13:35:40 +0800

    feat:  支持header和config

--- lib/src/build/script.ts
+++ lib/src/build/script.ts
@@ -10,6 +10,17 @@ type BuildOptions = {
 	path: string;
 };
 
+type BuildInfo = {
+	buildOptions: BuildOptions;
+
+	paths: {
+		headerfile: string;
+		configfile: string;
+		srcdir: string;
+		styledir: string;
+		htmldir: string;
+	}
+}
 function build(opt: BuildOptions) {
 	// const loadInfoFn = import(pathJoin(opt.path, "index.js"));
 	// const info = loadInfoFn.default()
@@ -20,6 +31,21 @@ function build(opt: BuildOptions) {
 	const stylefile = (name: string) => pathJoin(styledir, name);
 	const htmldir = pathJoin(opt.path, "html");
 	const htmlfile = (name: string) => pathJoin(htmldir, name);
+	const headerfile = pathJoin(opt.path, "header");
+	const configfile = pathJoin(opt.path, "config.json");
+
+
+	const buildInfo: BuildInfo = {
+		buildOptions: opt,
+
+		paths: {
+			headerfile,
+			configfile,
+			srcdir,
+			styledir,
+			htmldir
+		}
+	}
 
 	let styleCode = "";
 	for (const file of fs
@@ -27,7 +53,7 @@ function build(opt: BuildOptions) {
 		.filter((file) => file.endsWith(".condition.sass"))) {
 		let sassStyle = fs.readFileSync(stylefile(file), "utf8");
 
-        const cssStyle = `'${csso.minify(sass.compile(stylefile(file)).css).css}'`;
+		const cssStyle = `'${csso.minify(sass.compile(stylefile(file)).css).css}'`;
 		/{{([a-zA-Z0-9_]+):([a-zA-Z0-9_]+)}}/.exec(sassStyle)?.map((x) => {
 			const key = x[1];
 			const value = x[2];
@@ -40,36 +66,46 @@ function build(opt: BuildOptions) {
 	for (const file of fs
 		.readdirSync(srcdir)
 		.filter((file) => file.endsWith(".js"))) {
-		const code = processSingleJs(file, jsfile(file),{
-			opt,
-			srcdir,
-			styledir,
-			htmldir
-		});
+		const code = processSingleJs(file, jsfile(file), buildInfo);
 		jsCodemap.set(file, code);
 	}
 
-	const fullCode = getFullCode(jsCodemap, styleCode);
+	const fullCode = getFullCode(jsCodemap, styleCode, buildInfo);
 
 	writeScrpt(opt, fullCode);
 	// debugger;
 }
 
-function processSingleJs(filename: string, path: string, {opt, srcdir, styledir, htmldir}: {opt: BuildOptions, srcdir: string, styledir: string, htmldir: string}):string {
+function processSingleJs(filename: string, path: string, buildInfo: BuildInfo): string {
 	let code = fs.readFileSync(path, "utf8").trim();
 	if (code === "") return "\n";
 
 
-    const regex_STYLE = /\$STYLE\((.+?)\)/g;
-    code = code.replace(regex_STYLE, (_match, sassFileName) => {
-        sassFileName = sassFileName.replaceAll(' ', '').replaceAll('"', '').replaceAll("'", '');
-        if (!sassFileName.endsWith('.sass')) sassFileName += '.sass';
-        let sassFilePath = pathJoin(styledir, sassFileName);
-        const result = `'${csso.minify(sass.compile(sassFilePath).css).css}'`
-        return result;
-    });
+	const regex_STYLE = /\$STYLE\((.+?)\)/g;
+	code = code.replace(regex_STYLE, (_match, sassFileName) => {
+		sassFileName = sassFileName.replaceAll(' ', '').replaceAll('"', '').replaceAll("'", '');
+		if (!sassFileName.endsWith('.sass')) sassFileName += '.sass';
+		let sassFilePath = pathJoin(buildInfo.paths.styledir, sassFileName);
+		const result = `'${csso.minify(sass.compile(sassFilePath).css).css}'`
+		return result;
+	});
 
+	const key = filename.substring(0, filename.indexOf("."));
 	let result = "\n// " + filename + "\n";
+	try {
+		const evalCode = eval(code);
+		if (!evalCode.key) {
+			// 在第一个{后添加key:filename.substring(0, filename.firstIndexOf("."))
+			code = code.substring(0, code.indexOf("{") + 1) + `\nkey: "${key}", ` + code.substring(code.indexOf("{") + 1);
+		}
+		if (evalCode.showInMenu) {
+			result += `addOptionOnMenu("${key}")\n`
+		}
+		logger.log(`processSingleJs: ${path}, evalCode: ${evalCode}`);
+	} catch (error) {
+		logger.err(`processSingleJs: ${path}`, error as Error);
+	}
+
 	result += `addModule${code}\n`
 
 	if (filename.endsWith(".onclick.js")) {
@@ -80,19 +116,9 @@ function processSingleJs(filename: string, path: string, {opt, srcdir, styledir,
 
 	}
 
-	try {
-		const evalCode = eval(code);
-		if (evalCode.showInMenu) {
-			result += `addOptionOnMenu("${filename.substring(0, filename.lastIndexOf("."))}")\n`
-		}
-		logger.log(`processSingleJs: ${path}, evalCode: ${evalCode}`);
-	} catch (error) {
-		logger.err(`processSingleJs: ${path}`, error as Error);
-	}
-
 	return result;
 }
-function getFullCode(codemap: Map<string, string>, styleCode: string) {
+function getFullCode(codemap: Map<string, string>, styleCode: string, buildInfo: BuildInfo) {
 
 	let code_direct = "";
 	let code_onclick = "";
@@ -109,7 +135,13 @@ function getFullCode(codemap: Map<string, string>, styleCode: string) {
 
 	const code_utils = res.get_core_utils()
 
-	const fullCode = code_utils + styleCode + code_direct + code_onclick + code_mode;
+	const headerStr = fs.readFileSync(buildInfo.paths.headerfile, "utf8") + "\n"
+	const config = fs.readJsonSync(buildInfo.paths.configfile, "utf8");
+	const configVarStr = `var config = ${JSON.stringify(config, null, 4)};\n`
+
+
+	const fullCode = headerStr
+		+ `(function() {\n${configVarStr + code_utils + styleCode + code_direct + code_onclick + code_mode}\n})();\n`;
 	return fullCode;
 }
 
