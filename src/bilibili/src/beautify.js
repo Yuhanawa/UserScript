dcbef17531473a0c1842f2675d7f117739bc3e5e
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-07-13 15:09:24 +0800

    feat: 删除护眼蒙版功能

--- src/bilibili/src/beautify.js
+++ src/bilibili/src/beautify.js
@@ -2,20 +2,10 @@
   '已开启$on': () => {
     if (location.href === 'https://www.bilibili.com/' && get('bilibili_beautify_work_on_index', 'on') === 'off') return;
 
-    if (get('bilibili_eye_protection_cover', 'on') === 'on') {
-      style('html,:root{--bodybackground: ' +
-        (window.matchMedia("(prefers-color-scheme: dark)").matches
-          ? get('bilibili_eye_protection_cover_dark', `rgb(0 0 0 / 23%)`)
-          : get('bilibili_eye_protection_cover_light', `rgb(102 204 255 / 23%)`)) + '}'
-      )
-    } else {
-      style('html,:root{--bodybackground:transparent}')
-    }
-
-    if (get('bilibili_video_radius', 'on') === 'on') {
+    if (get('bilibili_video_radius')) {
       style($SASS('beautify.video.sass'))
     }
-    if (get('bilibili_background', 'on') === 'on') {
+    if (get('bilibili_background')) {
       style(`html,:root{--background:url(${get('bilibili_background_value')})}`)
     }
     return $SASS('beautify.sass')
