abf5a56f3fff5aa391d99f85f93c4c446a5c89a1
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-07-13 14:57:49 +0800

    fix: 删除异常的模块

--- userscripts/bilibili/src/remove_carousel.direct.js
+++
@@ -1,6 +0,0 @@
-({
-    name: '移除轮播图',
-    pages: ["home"],
-    showInMenu: true,
-    value: ".recommended-swipe{display:none;}"
-})
--- userscripts/bilibili/src/remove_floor_single_card.js
+++
@@ -1,6 +0,0 @@
-({
-    name: '移除首页特殊卡片',
-    pages: ["home"],
-    showInMenu: true,
-    value: ".floor-single-card{display:none;}"
-})
