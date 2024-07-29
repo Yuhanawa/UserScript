58f6274c24042ff7b6e9a2da98efa789ba9c4e98
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-07-29 15:15:54 +0800

    feat(csdn): 添加icon样式的设置按钮

--- userscripts/csdn/src/setting_btn.js
+++ userscripts/csdn/src/setting_btn.js
@@ -1,17 +1,34 @@
 ({
 	pages: ["article"],
-	value: () => {
-		delay(() => {
-			const articleTitleBox =
-				document.getElementsByClassName("article-title-box")[0];
-			const settingButton = document.createElement("a");
-			settingButton.innerText = "脚本设置";
-			settingButton.onclick = () => {
-				openConfigPanel();
-			};
-			settingButton.style =
-				"float: right;margin: 12px;font-size: 20px;text-decoration: underline !important;color: #4ea1db;";
-			articleTitleBox.insertAdjacentElement("afterbegin", settingButton);
-		}, 200);
+	value: {
+		text: () => {
+			delay(() => {
+				const articleTitleBox =
+					document.getElementsByClassName("article-title-box")[0];
+				const settingButton = document.createElement("a");
+				settingButton.innerText = "脚本设置";
+				settingButton.onclick = () => {
+					openConfigPanel();
+				};
+				settingButton.style =
+					"float: right;margin: 12px;font-size: 20px;text-decoration: underline !important;color: #4ea1db;";
+				articleTitleBox.insertAdjacentElement("afterbegin", settingButton);
+			}, 200);
+		},
+		icon: () => {
+			delay(() => {
+				const articleTitleBox =
+					document.getElementsByClassName("article-title-box")[0];
+				const settingButton = document.createElement("a");
+				settingButton.innerText = "⚙️";
+				settingButton.onclick = () => {
+					openConfigPanel();
+				};
+				settingButton.style =
+					"float: right;margin: 12px;font-size: 20px;text-decoration: none !important;color: #4ea1db;";
+				articleTitleBox.insertAdjacentElement("afterbegin", settingButton);
+			}, 200);
+		},
+		off: null,
 	},
 });
