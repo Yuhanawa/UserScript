cf008086cf71c9f9d8ec7b39ac157797410c4634
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-07-30 12:04:27 +0800

    feat(setting): 添加自定义icon功能

--- setting_page/src/script.js
+++ setting_page/src/script.js
@@ -117,9 +117,7 @@ function generateCategoryTabs() {
 		categoryItem.className =
 			"cursor-pointer inline-flex items-center px-4 py-3 text-white bg-blue-700 rounded-lg active w-full dark:bg-blue-600";
 		categoryItem.id = `category-${item.key}-tab`;
-		categoryItem.innerHTML = `<svg class="w-4 h-4 me-2 text-white" aria-hidden="true" xmlns="http://www.w3.org/2000/svg" fill="currentColor" viewBox="0 0 20 20">
-                  <path d="M10 0a10 10 0 1 0 10 10A10.011 10.011 0 0 0 10 0Zm0 5a3 3 0 1 1 0 6 3 3 0 0 1 0-6Zm0 13a8.949 8.949 0 0 1-4.951-1.488A3.987 3.987 0 0 1 9 13h2a3.987 3.987 0 0 1 3.951 3.512A8.949 8.949 0 0 1 10 18Z"/>
-        </svg>  <span>${item.display}</span>`;
+		categoryItem.innerHTML = `<div class="w-5 h-5 m-1 overflow-hidden">${item.icon}</div> <span>${item.display}</span>`;
 
 		const contentItem = document.createElement("div");
 		contentItem.className = "content-container-item hidden";
