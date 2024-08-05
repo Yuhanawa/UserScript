456c0c571f1e25d631371673085392822953329a
Yuhanawa <74097164+Yuhanawa@users.noreply.github.com>
2024-08-05 16:11:15 +0800

    fix(csdn): 现在blog-content-box-opacity-value会从配置中获取默认值

--- userscripts/csdn/src/background.js
+++ userscripts/csdn/src/background.js
@@ -5,7 +5,7 @@
 			style(`body{background:url("${get("background-value")}")}`);
 		}
 		style(`body{--blog-content-box-background:${get("blog-content-box-background-value")}}`);
-		style(`body{--blog-content-box-opacity:${get("blog-content-box-opacity-value", "0.98")}}`);
+		style(`body{--blog-content-box-opacity:${get("blog-content-box-opacity-value")}}`);
 		style(`body{--blog-header-box-background:${get("blog-header-box-background-value")}}`);
 		return $STYLE("background");
 	},
